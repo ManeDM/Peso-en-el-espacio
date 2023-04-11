584dc04bb4b41b9acbd5a020671c77c4dcf91742
ManeDM <mane657@hotmail.com>
2023-04-11 16:44:32 -0500

    Changing Styles

--- ts_files/peso_en_el_espacio.js
+++ ts_files/peso_en_el_espacio.js
@@ -1,32 +1,28 @@
 function calcular() {
-    var peso_tierra = document.getElementById("peso_init").value;
+    var peso_tierra = parseInt(document.getElementById("peso_init").value);
 
-    var calculo_lun = parseInt(peso_tierra) * 1.66 / 9.81;
+    var calculo_lun = Math.round(peso_tierra * 1.66 / 9.81) + " Kg";
     document.getElementById('caja_lu').innerHTML = calculo_lun;
 
-    var calculo_mer = parseInt(peso_tierra) * 3.7 / 9.81;
+    var calculo_mer = Math.round(peso_tierra * 3.7 / 9.81) + " Kg";
     document.getElementById('caja_me').innerHTML = calculo_mer;
     
-    var calculo_ven = parseInt(peso_tierra) * 8.87 / 9.81;
+    var calculo_ven = Math.round(peso_tierra * 8.87 / 9.81) + " Kg";
     document.getElementById('caja_ve').innerHTML = calculo_ven;
     
-    var calculo_mar = parseInt(peso_tierra) * 3.71 / 9.81;
+    var calculo_mar = Math.round(peso_tierra * 3.71 / 9.81) + " Kg";
     document.getElementById('caja_mar').innerHTML = calculo_mar;
     
-    var calculo_jup = parseInt(peso_tierra) * 24.79 / 9.81;
+    var calculo_jup = Math.round(peso_tierra * 24.79 / 9.81) + " Kg";
     document.getElementById('caja_jup').innerHTML = calculo_jup;
 
-    var calculo_sat = parseInt(peso_tierra) * 10.44 / 9.81;
+    var calculo_sat = Math.round(peso_tierra * 10.44 / 9.81) + " Kg";
     document.getElementById('caja_sat').innerHTML = calculo_sat;
 
-    var calculo_ur = parseInt(peso_tierra) * 8.69 / 9.81;
+    var calculo_ur = Math.round(peso_tierra * 8.69 / 9.81) + " Kg";
     document.getElementById('caja_nep').innerHTML = calculo_ur;
 
-    var calculo_nep = parseInt(peso_tierra) * 11.15 / 9.81;
-    document.getElementById('caja_ur').innerHTML = calculo_nep;
-     
+    var calculo_nep = Math.round(peso_tierra * 11.15 / 9.81) + " Kg";
+    document.getElementById('caja_ur').innerHTML = calculo_nep;   
 }
-
-
-    
     
